f69f6a6161a7335b8a9ec103584d7327ca78d693
Alexander Divers <alexanderdivers@Alexanders-MacBook-Pro.local>
2017-06-21 19:19:09 -0400

    adding firebase

---
+++ EmpDataManagement.js
@@ -0,0 +1,11 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBGJ481PEl1pSk6Z06BX6HKd-YsL-klRA8",
+    authDomain: "empdat.firebaseapp.com",
+    databaseURL: "https://empdat.firebaseio.com",
+    projectId: "empdat",
+    storageBucket: "",
+    messagingSenderId: "155340612682"
+  };
+  firebase.initializeApp(config);
