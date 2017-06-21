f70919b447ecf7a0a60b204b0f9c8bd406ea85a5
Carl Dor <carljdor@gmail.com>
2017-06-21 19:50:49 -0400

    add push to firebase

--- EmpDataManagement.js
+++ EmpDataManagement.js
@@ -1,4 +1,4 @@
-
+$(document).ready(function() {
   // Initialize Firebase
   var config = {
     apiKey: "AIzaSyBGJ481PEl1pSk6Z06BX6HKd-YsL-klRA8",
@@ -8,4 +8,35 @@
     storageBucket: "",
     messagingSenderId: "155340612682"
   };
+
   firebase.initializeApp(config);
+
+  var database = firebase.database();
+  var employee;
+  var role;
+  var startDate;
+  var monthlyRate;
+
+  console.log("the page loaded");
+
+  $(document).on("click", "#btnSubmit", function(event){
+
+  	event.preventDefault();
+
+  	console.log("clicking");
+
+  	employee = $("#employee-name").val().trim();
+  	role = $("#role").val().trim();
+  	startDate = $("#startdate").val().trim();
+  	monthlyRate = $("#monthlyrate").val().trim();
+
+  	database.ref().push({
+  		employee:employee,
+  		role:role,
+  		startDate:startDate,
+  		monthlyRate: monthlyRate
+  	});
+
+  });
+
+});
